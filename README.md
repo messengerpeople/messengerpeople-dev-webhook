36e0084bed95d7c42187d8cf92abb57fdc2848d3
Maximilian Tietz <info@mt-edv.de>
2019-10-21 10:29:57 +0200

    Simple PHP webhook - first commit.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # simple-php-webhook
+
+Simple webhook, simple documentation.
+This is an example webhook for PHP. It's not pretty, but it does the job. 
+
+We strongly suggest to keep webhooks simple and fast and handle processing logic async. This webhook contains an example message for testing. You can navigate to https://app.messengerpeople.dev/monitoring/ to see all your incoming and outgoing messages. The field "webhook_request" contains the payload that would be sent to your webhook.  
+
+DO:
+
+Store the incoming message in a messagequeue or database and process it asynchronously in a different worker.
+
+DON'T:
+
+Parse the message, do many DB Queries, or any blocking external requests. We have a very low timeout setting (2 seconds), so keep your webhook fast.
+
---
+++ webhook.php
@@ -0,0 +1,79 @@
+<?php
+header("Content-Type: application/json");
+define("VERIFICATION_CODE", "your-verification-code");
+define("SECRET", "your-secret");
+// Set this to true to use the example body.
+define("USE_EXAMPLE_BODY", false);
+
+if (SECRET && SECRET !== '') {
+    if (!isset($_SERVER['HTTP_AUTHORIZATION']) || $_SERVER['HTTP_AUTHORIZATION'] !== "Bearer ".SECRET) {
+        http_response_code(401);
+        die('Not authorized.');
+    }
+}
+
+// Read the request body
+$request_body = file_get_contents('php://input');
+
+// MessengerPeople Example Body
+$example_body = '{"uuid":"abcdef12-1234-aaaa-4321-abcdef123456","sender":"491721234567","recipient":"ab654321-4321-abcd-4321-987654321abc","payload":{"timestamp":"1571385584","text":"Hello World","user":{"id":"491721234567","name":"Tappy Tester","image":""},"attachment":"","type":"text"},"outgoing":false,"processed":null,"sent":null,"received":null,"read":null,"created":null,"messenger":"WB","messenger_id":"ABEGSRYyNxVAAhD2YdoUOWfO9YXSOWmnigB8"}';
+
+if (USE_EXAMPLE_BODY) {
+    $request_body = $example_body;
+}
+
+$response = [
+    "success" => true
+];
+
+// If request body is not empty
+if ($request_body) {
+
+    // Create associative array $payload from $request_body
+    $payload = json_decode($request_body, true);
+
+    // Check if MessengerPeople sent you a challenge, check the verification code. If both is correct, fine - if not - send a 403.
+    if (isset($payload['challenge']) && isset($payload['verification_code'])) {
+
+        $verification_code = $payload['verification_code'];
+
+        // If verification code does not match the value set by you, send 403 - forbidden.
+        if ($verification_code !== VERIFICATION_CODE) {
+            http_response_code(403);
+            die("Wrong verification code.");
+        }
+
+        // Add challenge to the response.
+        $response['challenge'] = $payload['challenge'];
+    }
+
+    // At this point everything is fine, the challenge was set, the secret header was checked and you can process the message.
+    processMessage($payload);
+
+    // Send a 200 response, so the MessengerPeople servers know, that everything arrived.
+    http_response_code(200);
+    echo json_encode($response);
+}
+
+
+/**
+ * Contains examples of what to do with the message and prints content to error log.
+ * We strongly suggest to keep webhooks simple and fast and handle processing logic async.
+ *
+ * @param $message
+ * @return bool
+ */
+function processMessage($message) {
+
+    // Store in MessageQueue or Database
+    saveToDatabase($message);
+
+    // Output the message to the error log.
+    error_log("[MESSENGERPEOPLE] Message received: ".print_r($message, true));
+
+    return true;
+}
+
+function saveToDatabase($message) {
+    return true;
+}
